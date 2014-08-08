ce706cbee1b03c52c6447036442c46233d3d7925
Peterson <Peterson@161.23.208.209>
2014-08-08 15:16:51 +0100

    historyfragment

---
+++ ChallengeMe/src/com/qmul/project/challengeme/HistoryFragment.java
@@ -0,0 +1,19 @@
+package com.qmul.project.challengeme;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+
+public class HistoryFragment extends Fragment {
+	
+	private static final String TAG = "SelectionFragment";
+	
+	public View onCreateView(LayoutInflater inflater, 
+	        ViewGroup container, Bundle savedInstanceState) {
+	    super.onCreateView(inflater, container, savedInstanceState);
+	    View view = inflater.inflate(R.layout.history, container, false);
+	    return view;
+	}
+}
