6d7f0154f82fbb4b2fd0f9f0584ea4d1a6d1318d
felipeaamaral <amaral.felipeaugusto@gmail.com>
2014-08-24 00:52:53 +0100

    upando again

--- ChallengeMe/src/com/qmul/project/challengeme/SportListDialog.java
+++ ChallengeMe/src/com/qmul/project/challengeme/SportListDialog.java
@@ -49,8 +49,6 @@ public class SportListDialog extends DialogFragment {
 								
 							}
 							
-							 
-							
 							((ChallengeMeApplication) getActivity()
 						             .getApplication())
 						             .setIsSportSelected(isSelected);
